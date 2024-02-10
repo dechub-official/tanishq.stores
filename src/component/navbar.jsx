c2de8e16cdddc69eee582be7a444de7409d03f6c
sudhirtawaniya <sudhir@yrpitsolutions.com>
2024-02-10 11:03:37 +0530

    done

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -320,7 +320,7 @@ centeredSlides={true}
                             1200: { slidesPerView: 3.2,spaceBetween:0, autoplay: true }, 
                             568: { slidesPerView: 3.1,spaceBetween:0, autoplay: true },
                             400: { slidesPerView: 2.2,spaceBetween:0, autoplay: true }, 
-                            360: { slidesPerView: 2.1, spaceBetween:20,slidesPerGroup: 1 },
+                            360: { slidesPerView: 2, spaceBetween:20,slidesPerGroup: 1 },
                             300: { slidesPerView: 1.8, spaceBetween:20,slidesPerGroup: 1 },
                             0: { slidesPerView: 1, spaceBetween:20,slidesPerGroup: 1 }
                         }}
