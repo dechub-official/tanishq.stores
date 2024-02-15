952647ee056735cdf0315464b2461e8d443286b4
sudhirtawaniya <sudhir@yrpitsolutions.com>
2024-02-15 11:26:22 +0530

    data save

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -3,14 +3,14 @@ import Home from "./pages/home";
 import Detailed from "./pages/detail";
 import CityPage from "./pages/city";
 import Page_Not_Found from "./pages/404";
-import Collection from "./pages/collection";
+// import Collection from "./pages/collection";
 import CelesteCollection from "./pages/celesteCollection";
 
 export default function Routing() {
     return <>
     <Routes>
         <Route exact element={<Home/>} path="/"/>
-        <Route exact element={<Collection/>} path="/store-locator/jewellery-stores/collections/kakatiya"/>
+        {/* <Route exact element={<Collection/>} path="/store-locator/jewellery-stores/collections/kakatiya"/> */}
         <Route exact element={<CelesteCollection/>} path="/store-locator/jewellery-stores/collections/celeste"/>
         <Route element={<Page_Not_Found/>} path="*"/>
         <Route element={<Detailed/>} path="/store-locator/jewellery-stores/:state/:city/:storename"/>
