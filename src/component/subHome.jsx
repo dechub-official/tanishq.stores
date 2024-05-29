7ec9c73bc40de4e7e7874377e0e3b62bdf56c5c0
sudhirtawaniya <sudhir@yrpitsolutions.com>
2024-05-29 17:33:06 +0530

    data changes

--- src/component/subHome.jsx
+++ src/component/subHome.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from 'react-router-dom'
 import { Autoplay } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import bg from '../assets/images/navbar/strip.jpg'
+import bg from '../assets/images/navbar/strip.mp4'
+import mobile_bg from '../assets/images/navbar/mobile.mp4'
+import goldbg from '../assets/images/navbar/strip.jpg'
 import kalsh from '../assets/images/navbar/exchange.png'
 import { useState } from 'react'
 // import SubHomeexp from '../assets/images/subhomeExpender.png'
@@ -9,9 +11,9 @@ export default function SubHome() {
     // const nav=useNavigate()
     const [open,setOpen]=useState(true)
     return <>
-      { open&& <div    id="dashBanner"  className="w-full home-top-banner cursor-pointer text-center text-white flex  justify-center items-center   relative"  >
+   {/* { open&& <div    id="dashBanner"  className="w-full home-top-banner cursor-pointer text-center text-white flex  justify-center items-center   relative"  >
 
-        {/* <p className='md:text-[white] text-[#ffffffe2] absolute right-1 md:right-5 cursor-pointer md:text-[16px] text-[14px] z-50 font-[600]' onClick={()=>setOpen(false)}>X</p> */}
+        <p className='md:text-[white] text-[#ffffffe2] absolute right-1 md:right-5 cursor-pointer md:text-[16px] text-[14px] z-50 font-[600]' onClick={()=>setOpen(false)}>X</p>
         <img src={kalsh} className='md:max-w-[40px] z-50 absolute left-3  max-w-[20px] max-h-[16px] md:max-h-[30px] mr-1' alt="" /> 
             <Swiper
                 slidesPerView={1}
@@ -40,10 +42,16 @@ export default function SubHome() {
                 <SwiperSlide   className=' !bg-[100%,10%]  max-h-[40px]  !bg-transparent !flex text-[white]  font-[600]' ><div className="flex items-center w-full justify-center  max-h-[40px] ">
                       <p  className='bg-transparent text-white whitespace-nowrap  text-[10px] md:px-2 md:text-[16px] md:font-[600]  md:w-auto'> Tanishq Festival of exchange is live </p></div></SwiperSlide>
                 <SwiperSlide  className='!bg-[100%,100%]  max-h-[40px] !bg-transparent text-[white] font-[600]' >   <p  className='bg-transparent text-[white] font-[600] text-[13.5px] px-2 md:text-[14px]   max-h-[40px]  md:w-auto'>Get 100% value. Click to get more details.</p></SwiperSlide>
-                {/* <SwiperSlide >  <TanishqService heading={"Buy New Jewellery"} subheading={"It can be any jewellery"} img={chain}/></SwiperSlide> */}
+                
 
             </Swiper>
-        </div>}
+        </div>}  */}
+        {/* <img src={bg} className='font-[600] max-h-[40px] w-full !my-auto' alt="" /> */}
+        <div className='home-top-banner cursor-pointer' id="dashBanner">
+        <video src={bg} className='max-h-[40px] md:block hidden w-full object-cover' muted loop  autoPlay></video>
+        <video src={mobile_bg} className='max-h-[40px] md:hidden block w-full object-cover' muted loop  autoPlay></video>
+        </div>
+       
         {/* <div className="w-full  text-center text-white flex md:hidden justify-center items-center   "  >
 
 
@@ -74,6 +82,6 @@ export default function SubHome() {
                 <SwiperSlide className='!bg-[#a37f29] text-[white] font-[600]' >   <p className='!bg-[#a37f29] text-[white] font-[600] text-[13.5px] px-2 md:text-[14px] font-[500]  md:w-auto'>Get 100%* value.&nbsp; <u className='font-[600] cursor-pointer' onClick={() => { window.location.href = "https://www.tanishq.co.in/exchange" }}>Read more</u></p></SwiperSlide>
               
             </Swiper>
-        </div> */}
+        </div>  */}
     </>
 }
