671db0c1b0d06002aac8e5140dc7789db018efd9
sudhirtawaniya <sudhir@yrpitsolutions.com>
2024-09-19 11:18:21 +0530

    status updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {
-      backgroundImage:{
-        tanishqAIBg:'linear-gradient(90deg, rgba(216,70,88,1) 0%, rgba(114,37,46,1) 100%);',
-        liteLimeBg:'linear-gradient(9deg, rgba(249,242,229,0.4901610302324054) 0%, rgba(249,242,229,1) 100%);'
-      },
-      boxShadow:{
-        sequreShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px;',
-
-      }
+      
     },
   },
   plugins: [],
