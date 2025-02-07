ce94f611be4fe31bf898513fb1e1b44bcfedcce9
sudhirtawaniya <sudhirtawaniya30@gmail.com>
2025-02-07 09:43:58 +0000

    celesteSearch url updated

--- src/pages/celesteCollection.jsx
+++ src/pages/celesteCollection.jsx
@@ -67,7 +67,7 @@ export default function CelesteCollection() {
       
             try {
                 setPosition([])
-                const data = await get(`/celesteAllStroes`)
+                const data = await get(`/celesteAllStores`)
                 // const data = await get(`/details?storeCode=&city=mumbai`)
                 // const data=await axios.get(`http://10.54.4.78:8129/stores/tanishq/search?value=${e.target.value}`)
                     // if(!data.data.result)
