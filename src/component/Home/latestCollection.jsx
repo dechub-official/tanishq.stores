e336ecae380127a5fce33094b21f53274f36bead
Sudhir <67323277+sudhirtawaniya@users.noreply.github.com>
2025-04-07 21:16:59 +0530

    Update latestCollection.jsx

--- src/component/Home/latestCollection.jsx
+++ src/component/Home/latestCollection.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
 export default function LatestCollection({img,link}) {
+   const BookAppointment = () => {
+        const newWindow = window.open(link, '_blank', 'noopener,noreferrer');
+        if (newWindow) newWindow.opener = null;
+
+   }
     return <>
-  <Link to={link}> <div className="md:max-w-[350px] md:min-w-[100px] min-h-[210px]  md:min-h-[530px] md:max-h-[530px]">
+  <div onClick={BookAppointment}> <div className="md:max-w-[350px] md:min-w-[100px] min-h-[210px]  md:min-h-[530px] md:max-h-[530px]">
     <img src={img} alt="" className="rounded-[25px]  md:min-h-[530px] min-h-[210px] md:max-h-[530px] h-full" />
     </div>
-    </Link> 
+  </div> 
     </>
 }
