8c575d9be3f41481bdb919a805b31ee98f448644
sudhirtawaniya <sudhirtawaniya30@gmail.com>
2025-05-23 06:25:09 +0100

    gif updated

--- src/component/Home/latestCollection.jsx
+++ src/component/Home/latestCollection.jsx
@@ -12,8 +12,8 @@ export default function LatestCollection({ img, link }) {
 
   }
   return <>
-    <div onClick={BookAppointment}> <div className="md:max-w-[350px] cursor-pointer md:min-w-[100px] min-h-[210px]  md:min-h-[530px] md:max-h-[530px]">
-      <img src={img} alt="" className="rounded-[25px]  md:min-h-[530px] min-h-[210px] md:max-h-[530px] h-full" />
+    <div onClick={BookAppointment}> <div className="md:max-w-[350px] cursor-pointer md:min-w-[100px] min-h-[210px] max-h-[210px]  md:min-h-[530px] md:max-h-[530px]">
+      <img src={img} alt="" className="rounded-[25px]  md:min-h-[530px] min-h-[210px] max-h-[210px] md:max-h-[530px] h-full" />
     </div>
     </div>
   </>
