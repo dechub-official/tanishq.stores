a851ebfdb76b47f2ec14a22256d0cbade6b90953
sudhirtawaniya <sudhirtawaniya30@gmail.com>
2025-06-05 16:51:03 +0100

    text responsiveness completed

--- src/component/Home/Banner.jsx
+++ src/component/Home/Banner.jsx
@@ -15,13 +15,14 @@ export default function Banner({ name, cardRef }) {
                 {/* <img src={DiwaliBannerImg} className="md:block hidden" alt="" /> */}
                 <img src={MobileBanner} className="md:hidden block" alt="" />
                 {/* <img src={DiwaliMobileBanner} className="md:hidden block" alt="" /> */}
-                <div className="md:w-[50%] top-[30%] md:top-[60%] md:left-[3%] md:transform-none w-full  absolute flex justify-center">
+                <div className="md:w-[50%] top-[30%] md:top-[60%] md:left-[3%] md:transform-none w-full  absolute flex flex-col items-center justify-center">
+                  
                     <p className=" text-[18px]   text-[rgba(119,7,10,0.70)] italic  font-serif  text-center font-[200] max-w-[250px]">Welcome to {name}</p>
+                 <p className=" text-[18px] md:block hidden   text-black italic  font-fraunces  text-center font-normal max-w-[390px]">Exchange festival is live, <br /> Get upto 2KT <sup> *</sup> extra on old gold value</p>
+               
                 </div>
 
-                <div className="md:w-[50%] top-[40%] md:top-[70%] md:left-[3%] md:transform-none w-full  absolute flex justify-center">
-                    <p className=" text-[18px]   text-black italic  font-fraunces  text-center font-normal max-w-[390px]">Exchange festival is live, <br /> Get upto 2KT <sup> *</sup> extra on old gold value</p>
-                </div>
+                
 
             </div>
             <AISummary cardRef={cardRef} />
