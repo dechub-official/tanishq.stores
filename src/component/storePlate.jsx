a18380174e2f68957967674595b0053cc3844916
sudhirtawaniya <sudhirtawaniya30@gmail.com>
2025-07-07 16:59:10 +0100

    popular city css overlap fixed

--- src/component/storePlate.jsx
+++ src/component/storePlate.jsx
@@ -110,7 +110,8 @@ export default function StorePlate({ data, setIndividualStoreData, index, setBoo
   //   console.log(checkTime());
 
   // },[])
-  const handleBookAnAppointment = () => {
+  const handleBookAnAppointment = (e) => {
+    e.stopPropagation();
     ReactGA.event({
       category: "Book An Appointment",
       storeName: data?.storeName,
@@ -144,7 +145,7 @@ export default function StorePlate({ data, setIndividualStoreData, index, setBoo
   }
   return (
     <>
-      <div class="col storeCard h-full md:max-w-[550px]"> <div class="card border h-full rounded-xl shadow-sm">
+      <div onClick={() => { handleStoreNavidation(data) }} class="col storeCard h-full md:max-w-[550px]"> <div class="card border h-full rounded-xl shadow-sm">
         <div class="flex items-center stag p-3 mb-2">
           {/* <i class="bi bi-shop me-2"></i> */}
           <p class="mb-0">
@@ -162,7 +163,7 @@ export default function StorePlate({ data, setIndividualStoreData, index, setBoo
         </div>
         <div class="flex gap-2 mt-8 px-1 md:px-3">
           <button
-            class="btn border-0 gap-1 rounded-pill bg-red-50 flex justify-center items-center px-2 py-1 pr-1 md:p-2"
+            class="btn relative z-10 border-0 gap-1 rounded-pill bg-red-50 flex justify-center items-center px-2 py-1 pr-1 md:p-2"
             onClick={handleBookAnAppointment}
           >
             <span class="pr-1 text-[9px] whitespace-nowrap md:text-[12px] font-[500]">BOOK AN APPOINTMENT</span>
@@ -184,57 +185,5 @@ export default function StorePlate({ data, setIndividualStoreData, index, setBoo
 
 
 
-    // <><div className="box bg-[white] md:min-w-[500px] min-w-[90%] max-w-[90%] sm:min-w-[310px] flex flex-col  md:max-w-[500px] min-h-[370px] md:max-h-[370px]  my-4 px-4  md:px-8 pt-4 rounded-[22px] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
-    //     <h2 className='text-[#832729] py-2 md:text-[18px] text-[15px] font-[700]'>{`Tanishq Jewellery - ${data.storeName}`}</h2>
-    //     <p className='pb-4 font-[700] text-[#832729]  border-b-[#832729] border-b-[3px] cursor-pointer' onClick={() => Navigate(`/store-locator/jewellery-stores/${data.storeState.toLowerCase().replace(" ", "-")}/${data.storeCity.toLowerCase().replace(" ", "-")}/${data.storeName.toLowerCase().split("- ")[1] ? data.storeName.toLowerCase().split("- ")[1].replace(/ /g, "-").replace(",", "-") : data.storeName.toLowerCase().replace(" ", "-").replace(",", "-")}-${data.storeCode}`)}>View Store Page{">>>"}</p>
-    //     <p className='my-2 text-[15px] max-w-[300px] min-h-[60px]  font-[500]'>{data.storeAddress}</p>
-    //     <p className='my-2 text-[15px] max-w-[300px] font-[500]'>Phone: {data.storePhoneNoOne}</p>
-    //     <p className='my-2 py-2 pb-0  mb-2 text-[15px] max-w-[300px] font-[500]'>{`Working Hours: ${data.storeOpeningTime?.replace(/.\d{2}\s/, ' ')} - ${data.storeClosingTime?.replace(/.\d{2}\s/, ' ')}`}</p>
-    //     <p className="border-b-[#832729] mb-2 border-b-[3px]"></p>
-
-    //     <div className="w-full bg-[#F2F2F2] text-center text-white flex  items-center   "  >
-
-
-
-    //     </div>
-
-    //     <div className="flex  mt-1  ">
-
-    //         <div className="flex w-[100%] ">
-    //             <div className="w-[50%] flex ">
-    //                 <Button name="Get Directions" iconstyle={"min-w-[15px] max-w-[15px] mr-1 w-[12px]"} style={'text-white bg-[#832729] font-500 text-[10px]  px-2'} onclick={() => {
-    //                     ReactGA.event({
-    //                         category: data.storeLocationLink,
-    //                         value: Clicks.getDir + 1,
-    //                         action: "Get Directions",
-    //                     });
-    //                     setClicks({ ...Clicks, getDir: Clicks.getDir + 1 })
-    //                     BookAppointment(data.storeLocationLink)
-    //                 }}
-    //                     icon={map}
-    //                 />
-    //             </div>
-    //             <div className="w-[60%] flex ">
-    //                 <Button name="Book an Appointment" iconstyle={"min-w-[14px] max-w-[14px]  mr-1"} style={'whitespace-nowrap text-white bg-[#832729] font-500 text-[10px] px-1'} onclick={() => {
-    //                     ReactGA.event({
-    //                         category: "Book An Appointment",
-    //                         value: Clicks.Bookan + 1,
-    //                         action: "Book An Appointment",
-    //                     });
-    //                     setClicks({ ...Clicks, Bookan: Clicks.Bookan + 1 })
-    //                     if (localStorage.getItem("nav-hide")) {
-    //                         handleAppPopup()
-    //                     }
-    //                     else
-    //                         BookAppointment("https://www.tanishq.co.in/book-an-appointment")
-    //                 }}
-    //                     icon={calender} />
-    //             </div>
-
-    //         </div>
-    //     </div>
-
-
-    // </div>
   )
 }
